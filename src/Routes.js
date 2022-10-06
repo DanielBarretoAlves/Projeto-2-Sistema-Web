381c190ebc9f00d02e67d8efd10465ef7277a736
Thiago Santos <thiagomtd2@gmail.com>
2022-10-06 16:56:04 -0300

    ajuste de rotas

--- src/Routes.js
+++ src/Routes.js
@@ -1,24 +1,30 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import HomePage from './pages/HomePage'
 
 const Routes = () => {
-    return (
-        <Switch>
-            <Route exact path="/">
-                <HomePage />
-            </Route>
-            <Route exact path="/statistics">
-                <h1>Statistics Page</h1>
-            </Route>
-            <Route exact path="/customers">
-                <h1>Customers Page</h1>
-            </Route>
-            <Route exact path="/diagrams">
-                <h1>Diagrams Page</h1>
-            </Route>
-        </Switch>
-    );
-};
+  return (
+    <Switch>
+      <Route exact path='/'>
+        <HomePage />
+      </Route>
+      <Route exact path='/duvidas'>
+        <h1>Duvidas Page</h1>
+      </Route>
+      <Route exact path='/materias'>
+        <h1>Materias Page</h1>
+      </Route>
+      <Route exact path='/testes'>
+        <h1>Testes Page</h1>
+      </Route>
+      <Route exact path='/reforco'>
+        <h1>Reforço Page</h1>
+      </Route>
+      <Route exact path='/login'>
+        <h1>Login Page</h1>
+      </Route>
+    </Switch>
+  )
+}
 
-export default Routes;
+export default Routes
