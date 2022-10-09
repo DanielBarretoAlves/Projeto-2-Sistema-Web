2f04cc45c815c644b95ed7e61d407bdd00521226
DanielBarretoAlves <danielbarreto559@gmail.com>
2022-10-09 13:24:16 -0300

    Form elements adicionados na tela questions

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,12 +1,18 @@
 const Question = () => {
-    return(
-      <>
-      <h2>Teste</h2>
-      </>
+  return (
+    <>
+      <div>
+        <h2>Teste</h2>
+        <b>Titulo</b>
+        <input type="text"></input>
+        <b>Informe sua Duvida</b>
+        <input type="textArea"></input>
+      </div>
+    </>
 
-    )
+  )
 
-    
-  }
 
-  export default Question
+}
+
+export default Question
