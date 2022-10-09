8548c8ef3247fc296f12d1889d16fa6a375b0b13
DanielBarretoAlves <danielbarreto559@gmail.com>
2022-10-09 13:38:30 -0300

    Sessão de Answers na tela Questions

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,12 +1,22 @@
 const Question = () => {
   return (
     <>
-      <div>
-        <h2>Teste</h2>
+      <h2>Teste</h2>
+      <div class="questions">
         <b>Titulo</b>
         <input type="text"></input>
         <b>Informe sua Duvida</b>
         <input type="textArea"></input>
+      </div>
+
+      <div class="answers">
+        <ul>
+          
+          <li>Vai ter um card aqui com titulo + Contudo</li>
+          <li>Vai ter um card aqui com titulo + Contudo</li>
+          <li>Vai ter um card aqui com titulo + Contudo</li>
+
+        </ul>
       </div>
     </>
 
