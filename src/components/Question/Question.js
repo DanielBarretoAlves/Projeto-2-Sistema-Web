35eede0229dbc1ddda84ce97c37b9184ce5e37ad
DanielBarretoAlves <danielbarreto559@gmail.com>
2022-10-15 11:58:38 -0300

    tryng to test method

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,14 +1,24 @@
-import {TextArea, Card } from './styles'
+import { TextArea, Card } from './styles'
 
 
 // criar JSON
 const questionList = '{"title":"Problema de Matematica", "contentBody":"Preciso de aprender a fazer calculo fatorial"}'
 
 
-function newQuestion(textTitle, textBody)
-{
+function newQuestion(textTitle, textBody) {
   const obj = JSON.parse(questionList);
-  obj.push({title:textTitle, contentBody:textBody})
+  obj.push({ title: textTitle, contentBody: textBody })
+
+  // --------------------------------------------------
+
+  var ul = document.getElementById("contentList");
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode("Four"));
+  li.setAttribute("id", "element4"); // added line
+  ul.appendChild(li);
+  alert(li.id);
+
+  // --------------------------------------------------
 }
 
 
@@ -17,28 +27,30 @@ function newQuestion(textTitle, textBody)
 const Question = () => {
   return (
     <>
-     
-        <h2>Nome do Usuario</h2>
-        <Card class="questions">
-          <form>
-            <p>Titulo:</p>
-            <input type="text"></input>
-            <p>Dúvida:</p>
-            <TextArea></TextArea>
-            <button type="submit">Enviar</button>
 
-          </form>
-        </Card>
+      <h2>Nome do Usuario</h2>
+      <Card class="questions">
+        <form>
+          <p>Titulo:</p>
+          <input type="text" id="title"></input>
+          <p>Dúvida:</p>
+          <TextArea id="contentBody"></TextArea>
+          {/* <button type="submit">Enviar</button> */}
 
-        <Card class="answers">
-          <ul id="contentList">
+          <input type="button" onClick="newQuestion(getElementbyId(title),getElementbyId(contentBody))">Confirmar</input>
 
-            <li><div>Vai ter um card aqui com titulo + Contudo</div></li>
-            <li><div>Vai ter um card aqui com titulo + Contudo</div></li>
-            <li><div>Vai ter um card aqui com titulo + Contudo</div></li>
+        </form>
+      </Card>
 
-          </ul>
-        </Card>
+      <Card class="answers">
+        <ul id="contentList">
+
+          <li><div>Vai ter um card aqui com titulo + Contudo</div></li>
+          <li><div>Vai ter um card aqui com titulo + Contudo</div></li>
+          <li><div>Vai ter um card aqui com titulo + Contudo</div></li>
+
+        </ul>
+      </Card>
 
     </>
 
