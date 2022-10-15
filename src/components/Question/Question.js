b66b689937d13914255beaacf6e8f7008e38a1fd
DanielBarretoAlves <danielbarreto559@gmail.com>
2022-10-15 13:55:11 -0300

    TODOs

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,42 +1,49 @@
 import { TextArea, Card } from './styles'
+import { useState } from 'react'
+
+
+// const questionList = '{"title":"Problema de Matematica", "contentBody":"Preciso de aprender a fazer calculo fatorial"}'
 
 
 
-const questionList = '{"title":"Problema de Matematica", "contentBody":"Preciso de aprender a fazer calculo fatorial"}'
+// TODO: passar user ID e receber lista de duvidas do USER.
 
-
-function getQuestion() {
-  // var title = document.getElementById("title").value
-  alert("teste")
-}
-
+// TODO: Cadastrar duvida no USER
 
 
 const Question = () => {
+  const [title, setTitle] = useState("")
+  const [question, setQuestion] = useState("")
+
+  function getQuestion(e) {
+    // var title = document.getElementById("title").value
+    e.preventDefault();
+    console.log(title, question)
+
+  }
   return (
     <>
       {/* ************************************************** */}
 
-     
+
 
       {/* ************************************************** */}
 
 
       <h2>Nome do Usuario</h2>
-      <Card class="questions">
-        <form>
+      <Card className="questions">
+        <form onSubmit={getQuestion}>
           <p>Titulo:</p>
-          <input type="text" id="title"></input>
-          <p id="test">Dúvida:</p>
-          <TextArea id="contentBody"></TextArea>
-          <button onclick={getQuestion()} type="submit">Enviar</button>
+          <input onChange={(e) => { setTitle(e.target.value) }}></input>
 
-
+          <p>Dúvida:</p>
+          <TextArea onChange={(e) => { setQuestion(e.target.value) }}></TextArea>
+          <button>Enviar</button>
 
         </form>
       </Card>
 
-      <Card class="answers">
+      <Card className="answers">
         <ul id="contentList">
 
           <li><div>Vai ter um card aqui com titulo + Contudo</div></li>
