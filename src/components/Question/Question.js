424965a995cd28bac2f2b0e73acab8f5592c5bed
DanielBarretoAlves <danielbarreto559@gmail.com>
2022-10-15 12:38:32 -0300

    teste

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,43 +1,38 @@
 import { TextArea, Card } from './styles'
 
 
-// criar JSON
+
 const questionList = '{"title":"Problema de Matematica", "contentBody":"Preciso de aprender a fazer calculo fatorial"}'
 
 
-function newQuestion(textTitle, textBody) {
-  const obj = JSON.parse(questionList);
-  obj.push({ title: textTitle, contentBody: textBody })
-
-  // --------------------------------------------------
-
-  var ul = document.getElementById("contentList");
-  var li = document.createElement("li");
-  li.appendChild(document.createTextNode("Four"));
-  li.setAttribute("id", "element4"); // added line
-  ul.appendChild(li);
-  alert(li.id);
-
-  // --------------------------------------------------
+function getQuestion() {
+  var title = document.getElementById("title").value
+  document.getElementById("test").innerHTML = "whatever";
+  alert("title")
 }
-
 
 
 
 const Question = () => {
   return (
     <>
+      {/* ************************************************** */}
+
+     
+
+      {/* ************************************************** */}
+
 
       <h2>Nome do Usuario</h2>
       <Card class="questions">
         <form>
           <p>Titulo:</p>
           <input type="text" id="title"></input>
-          <p>Dúvida:</p>
+          <p id="test">Dúvida:</p>
           <TextArea id="contentBody"></TextArea>
-          {/* <button type="submit">Enviar</button> */}
+          <button onclick="getQuestion()" type="submit">Enviar</button>
 
-          <input type="button" onClick="newQuestion(getElementbyId(title),getElementbyId(contentBody))">Confirmar</input>
+
 
         </form>
       </Card>
